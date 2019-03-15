fe474ee76134cbf5f645b964fa5f8fb0930e795c
Nick Skalenko <nikolasthebest839@gmail.com>
2019-03-15 22:09:55 +0300

    Modified Parser

--- parser.php
+++ parser.php
@@ -12,21 +12,21 @@ class Parser
     /**
      * Parser constructor.
      * @param string $url
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws Exception
      */
     public function __construct(string $url)
     {
         /** @var string $url */
-        $this->url = $url;
         $this->validate();
-        $this->getArrOfPictures();
+        $this->url = $url;
     }
 
     /**
      * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getArrOfPictures()
+    
+    public function getArrOfPictures()
     {
         preg_match_all('#src="[0-9a-z-/=.,?]+"#i', $this->getBody(), $this->pictures);
         return $this->pictures;
@@ -47,7 +47,7 @@ class Parser
      */
     private function isCorrectUrl()
     {
-        return preg_match('#^http[0-9a-z-=+_/\\\\:,.?]+\.[a-z]{2,3}/?$#i', $this->url);
+        return preg_match('#^https?[0-9a-z-=+_/\\\\:,.?]+\.[a-z]{2,3}/?$#i', $this->url);
     }
 
     /**
